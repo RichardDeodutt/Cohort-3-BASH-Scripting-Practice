2c0793a219b3627ae2076c3856398c96222002ce
Richard Deodutt <richarddeodutt@gmail.com>
2022-09-10 18:14:14 +0000

    More comments

--- Bash-Scripting-Exercise-2/Script.sh
+++ Bash-Scripting-Exercise-2/Script.sh
@@ -11,11 +11,15 @@
 #Read 'n'
 read -p "Enter a integer number: " n
 
+#Check to make sure n is a integer
 if [ -n "$n" ] && [ "$n" -eq "$n" ] ; then
+    #For loop to run the loop as many times as the 'n' variable
     for i in $( seq 1 $n ) ; do
+        #Echo the sequence of integers from 1 to the 'i' variable which can be considered a count of how many times the loop ran
         echo $( seq 1 $i )
     done
 else
+    #A integer was not entered by the user
     echo "You did not enter the number correctly"
 fi
 
